e37dc210204979dbadc14f51cfc25f861147d469
BaldinL <kn23-l.baldin@nubip.edu.ua>
2024-09-27 18:10:56 +0300

    modified:   tasks/task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,15 +1,8 @@
 //Функція filterAdults приймає масив об'єктів, що представлє людей (з полями name та age),
 //вам потрібно написати код, який повертає новий масив, який містить лише тих людей, чий вік більше або дорівнює 18.
 function filterAdults(people) {
- let newArr = people.filter((age)=> age>=18);
+ let newArr = people.filter((people)=> people.age>=18);
  console.log(newArr);
-  // let k=0;
-  // for(let i = 0;i<people.length;i++){
-  //   if(people[i].age>=18){
-  //     newArr[k]=people[i];
-  //     k++;
-  //   }
-  // }
    return newArr;
 }
 
